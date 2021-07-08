2e66c2bd6577d654c250421a8be1e680d3baf09b
Shreyansh Dawar <shreyanshdawar05@gmail.com>
2021-07-08 15:53:23 +0530

    new : TasksCompleted

---
+++ src/Components/TasksCompleted.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './TasksCompleted.css';
+
+function TasksCompleted(){
+    return(
+        
+        <div className="taskscompleted-area">
+          <h2 className="header"> Tasks List </h2>
+            <div className="tasks">
+            <h4> Task List </h4>    
+            </div>
+        </div>
+    );
+}
+export default TasksCompleted;
