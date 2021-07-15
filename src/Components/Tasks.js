85515ac3fbb7d688461b23a8bcbaab4073516f35
Shreyansh Dawar <shreyanshdawar05@gmail.com>
2021-07-15 18:20:20 +0530

    feat : submit on button click

--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -12,15 +12,24 @@ function Tasks() {
     
     }
 
+    function SubmitHandler(e){
+        
+        e.preventDefault();
+      
+        console.log("submitted");
+    }
+    
+    
     return(
         <div className="Tasks-card">
             <h1 className="Tasks-card-label">Type Your Tasks Here :</h1> 
             <div className="Tasks-body">
-                <div> <form >
+                <div> <form onSubmit={SubmitHandler} >
                    
                    <div className="input-field">    
                        <h1 className="task-label">Hey there ! </h1>
                        <input type="text" placeholder="what needs to be done ?" id="textblock1" onInput={CheckInput} size="50"/>
+                       <button className="submit-button" onClick={SubmitHandler}>+</button>
                    </div>
 
                      
