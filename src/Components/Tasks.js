99a5444a980c6a6c9c43d59a134cbb746913a089
Shreyansh Dawar <shreyanshdawar05@gmail.com>
2021-07-19 18:27:42 +0530

    style : background color

--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -1,46 +1,59 @@
 import React , {useState} from 'react';
-import './Tasks.css'
+import './Tasks.css';
 import TasksCompleted from './TasksCompleted';
 
 
-
 function Tasks() {
-   
-   
-    const [text,SetText]=useState(['']);
     
+    const [text,SetText]=useState(['']);
+    const [todos,SetTodos]=useState([]);
+    let sometext = [text];              
+    
+     
     
     function InputHandler(e){
-
-        SetText(e.target.value);
+        
+       localStorage.setItem("Task ",JSON.stringify(text));
+        SetText(e.target.value); 
         console.log('Got some Input !'+ e.target.value);
+        console.log(sometext);                  
+
     }
+
+    
 
     function SubmitHandler(e){
         
         e.preventDefault();
+        SetTodos([...todos,{
+            text:text,
+            completed:false,
+            id:Math.random()*1000
+        }])
+        SetText("");
         console.log("submitted");
     }
+    console.log(todos);
     
     
+    
+
     return(
         <div className="Tasks-card">
-            <h1 className="Tasks-card-label">Type Your Tasks Here :</h1> 
-            <div className="Tasks-body">
-                <div> <form onSubmit={SubmitHandler} >
+            <h1 className="Tasks-card-label">Type Your Tasks Here :</h1>
+             <div className="Tasks-body">
+                <div><form onSubmit={SubmitHandler}>
                    
                    <div className="input-field">    
-                       <h1 className="task-label">Hey there ! </h1>
-                       <input type="text" placeholder="what needs to be done ?" id="textblock1" onInput={InputHandler} size="50"/>
+                       <h2 className="task-label">Hey there ! </h2>
+                       <input type="text" value={sometext} placeholder="what needs to be done ?"  id="textblock1"  onChange={InputHandler}
+                         size="50"/>
                        <button className="submit-button" onClick={SubmitHandler}>+</button>
                    </div>
-   
+                  
                 </form> </div>
-
-                {text}
-
-            </div>
-                <TasksCompleted/>
+             </div>
+             <TasksCompleted todos={todos} SetTodos={SetTodos} settext={sometext} SubmitHandler={SubmitHandler}/> 
         </div>
     );
     
--- src/Components/TasksCompleted.js
+++ src/Components/TasksCompleted.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import './TasksCompleted.css';
+import SingleTask from "./SingleTask";
 
-function TasksCompleted(){
-    return(
+function TasksCompleted({todos,SetTodos})
+    {
         
-        <div className="taskscompleted-area">
-          <h2 className="header-text"> Tasks List </h2>
+       
+        return(
+            
+            <div className="taskscompleted-card">
+                <h2 className="header-text"> Tasks List </h2>
             <div className="tasks">
-            <h4 className="text"> following is a List of all the tasks you have : </h4>       
+             <h4 className="text"> following is a List of all the tasks you have : </h4>    
+               
+                  <div className="taskmap">
+                    
+                    
+                   {todos.map((todo) => (
+                       <SingleTask text={todo.text} todo={todo} todos={todos} SetTodos={SetTodos} key={todo.id}  />
+                   ))}
+                   
+                  </div> 
+                   
             </div>
-        </div>
+           
+            </div>
+
+
     );
 }
 export default TasksCompleted;
