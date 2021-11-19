fc53ef9fa948a1a31c5b3b39f8ef877b19902348
Shreyansh Dawar <shreyanshdawar05@gmail.com>
2021-11-19 17:34:21 +0530

    feat: task delete validation

--- src/Components/SingleTask.js
+++ src/Components/SingleTask.js
@@ -2,51 +2,63 @@ import React from 'react';
 import "./SingleTask.css";
 
 
-function SingleTask({text,todo,todos,SetTodos}){
-    
-   
-    function CheckboxHandler(){
-    
+function SingleTask({ text, todo, todos, SetTodos }) {
 
-        SetTodos(todos.map((item)=>{
-            if(/*checkBox.checked===true &&*/ item.id===todo.id) {
-                return{
-                    ...item,completed:!item.completed
+
+    function CheckboxHandler() {
+
+
+        SetTodos(todos.map((item) => {
+            if ( item.id === todo.id ) {
+                return {
+                    ...item, completed: !item.completed
                 }
             }
-            
+
             return item;
         }))
 
     }
 
-    function DeleteHandler(){
-        if(todo.completed===false){
-            alert("This Task is not yet done !");
-        }
+    function DeleteHandler() {
 
-        else{
-        SetTodos(todos.filter((el)=>el.id!==todo.id))
-        console.log("Deleted ToDo :"+todo.text);
-    
-        }
-    }
+        if (todo.completed === false) {
 
-    return(
-        <div className={todo.completed===true?"single-task":''}>
-             <div className="flex-div">
+                if( window.confirm("This Task is not yet done, are you sure you want to delete it ?") ){
+
+                todo.completed = true;
+                SetTodos(todos.filter((el) => el.id !== todo.id))
+                    
+                }          
             
-                <div className="main-div"> 
-                  <input type="checkbox"  id="checkbox" onClick={CheckboxHandler}/>
-                 {text}
+            // else{
+            //     alert("cancelled");
+            //  }
+            }
+
+        else {
+            SetTodos(todos.filter((el) => el.id !== todo.id))
+            console.log("Deleted ToDo :" + todo.text);
+
+             }
+      }
+    
+
+    return (
+        <div className={todo.completed === true ? "single-task" : ''}>
+            <div className="flex-div">
+
+                <div className="main-div">
+                    <input type="checkbox" id="checkbox" onClick={CheckboxHandler} />
+                    {text}
                 </div>
                 <div className="deletebtn-div">
                     <button className="delete-button" onClick={DeleteHandler}>-</button>
                 </div>
-            
-             </div>
+
+            </div>
         </div>
-    )
+    );
 
 }
 export default SingleTask;
